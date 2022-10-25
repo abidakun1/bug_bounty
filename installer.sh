98787e577ff5aec424eb7b19732e2253bc5df7fc
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-10-25 16:38:31 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,10 +6,8 @@ sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
 echo installing the must-have pre-requisites
-while read -r p ; do sudo apt-get update && sudo apt-get install -y  $LIST_OF_APPS; done  
-echo -e "\n"
-if [ $RESULT -eq 0 ]; then
-
-git clone https://github.com/Edu4rdSHL/findomain.git;cd findomain;cargo build --release;sudo cp target/release/findomain /usr/bin/
-
+while read -r p ; do sudo apt-get update && sudo apt-get install -y  $LIST_OF_APPS;
+git clone https://github.com/Edu4rdSHL/findomain.git;cd findomain;cargo build --release;sudo cp target/release/findomain /usr/bin/;
+done 
 echo "DONE"
+exit 
