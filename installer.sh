89de0a2e339c144184a441342eb6155473f9b528
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-10-25 17:27:14 -0700

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LIST_OF_APPS="amass figlet sublist3r subfinder cargo httpx-toolkit assetfinder nuclei"
+
+if [[ "$(whoami)" != root ]]; then
+  echo "Only sudo user can run this script."
+  exit 1
+echo "installing the must-have pre-requisites"
+while read -r p 
+do 
+sudo apt-get update && sudo apt-get install -y  $LIST_OF_APPS;
+
+git clone https://github.com/Edu4rdSHL/findomain.git;cd findomain;cargo build --release;sudo cp target/release/findomain /usr/bin/;
+
+done 
+
+echo "DONE"
+
+exit 0
