3786f8f3b5a61e07d3ac7cb26a11ab4af197eaca
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-11-05 05:31:15 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LIST_OF_APPS="amass dirsearch figlet sublist3r subfinder cargo httpx-toolkit assetfinder nuclei seclists"
+LIST_OF_APPS="amass dirsearch figlet sublist3r subfinder cargo httpx-toolkit assetfinder nuclei seclists httprobe aquatone"
 
 if [[ "$(whoami)" != root ]]; then
 echo "Only sudo user can run this script."
@@ -9,10 +9,10 @@ fi
 echo "Press EnterKey to start installing the must-have pre-requisetes"
 while read -r p 
 do 
-sudo apt-get update && sudo apt-get install -y  $LIST_OF_APPS;
-
+apt-get update && sudo apt-get install -y  $LIST_OF_APPS;
+go install github.com/lc/gau/v2/cmd/gau@latest
 git clone https://github.com/Edu4rdSHL/findomain.git;cd findomain && cargo build --release;sudo cp target/release/findomain /usr/bin/
-
+wget https://github.com/tomnomnom/unfurl/releases/download/v0.0.1/unfurl-linux-amd64-0.0.1.tgz;tar xzf unfurl-linux-amd64-0.0.1.tgz;sudo mv unfurl /usr/bin 
 done 
 
 echo "DONE"
