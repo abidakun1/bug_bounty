de5d2dbc9995f64e00262d1199cfc98a15b4075a
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-11-06 09:08:38 -0800

    Update enum.sh

--- enum.sh
+++ enum.sh
@@ -98,7 +98,7 @@ echo  "Total of $(wc -l $SUBDOMAIN_PATH/urllist.txt | awk '{print $1}') live sub
 
 printf "\n----- TIME TO TAKE SCREENSHOTS OF ALL PROBE SUBDOMAIN -----\n\n" 
 echo "Starting aquatone scan..."
-cat $SUBDOMAIN_PATH/urllist.txt | aquatone  -out $SCREENSHOT -threads 5 -silent 
+cat $SUBDOMAIN_PATH/urllist.txt | sudo aquatone  -out $SCREENSHOT -threads 5 -silent 
 
 
 printf "\n----- SCRAPE4SCRAPE -----\n\n" 
