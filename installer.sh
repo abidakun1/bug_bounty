a96908853da88875049be7b52e3d1edc4f959da4
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-11-12 07:45:59 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ echo "Press EnterKey to start installing the must-have pre-requisetes"
 while read -r p 
 do 
 apt-get update && sudo apt-get install -y  $LIST_OF_APPS;
-go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/lc/gau/v2/cmd/gau@latest; cp /root/go/bin/gau /usr/local/go/bin/
 wget https://github.com/tomnomnom/unfurl/releases/download/v0.0.1/unfurl-linux-amd64-0.0.1.tgz;tar xzf unfurl-linux-amd64-0.0.1.tgz;sudo mv unfurl /usr/bin 
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip;unzip aquatone_linux_amd64_1.7.0.zip;sudo mv aquatone /usr/bin
 git clone https://github.com/Edu4rdSHL/findomain.git;cd findomain && cargo build --release;sudo cp target/release/findomain /usr/bin/
