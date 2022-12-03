121b62b480f7a390dc5c7bbc078cc4f5398e4402
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-12-03 05:31:43 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,20 +1,61 @@
 #!/bin/bash
 
-LIST_OF_APPS="cargo amass dirsearch figlet sublist3r subfinder  assetfinder nuclei seclists httprobe aquatone dnsrecon httpx-toolkit"
+LIST_OF_APPS="cargo amass dirsearch  sublist3r subfinder  assetfinder nuclei seclists httprobe aquatone dnsrecon httpx-toolkit"
 
-if [[ "$(whoami)" != root ]]; then
+if [[ "$(whoami)" != "root" ]]; then
 echo "Only sudo user can run this script."
 exit 1
 fi
-echo "Press EnterKey to start installing the must-have pre-requisetes"
-while read -r p 
-do 
-apt-get update &&  apt-get install -y  $LIST_OF_APPS;
-go install github.com/lc/gau/v2/cmd/gau@latest; cp /root/go/bin/gau /usr/bin/
-wget https://github.com/tomnomnom/unfurl/releases/download/v0.0.1/unfurl-linux-amd64-0.0.1.tgz;tar xzf unfurl-linux-amd64-0.0.1.tgz;sudo mv unfurl /usr/bin 
-wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip;unzip aquatone_linux_amd64_1.7.0.zip;sudo mv aquatone /usr/bin
-git clone https://github.com/Edu4rdSHL/findomain.git;cd findomain && cargo build --release;sudo cp target/release/findomain /usr/bin/
-done 
+
+apt-get update &&  apt-get install -y  $LIST_OF_APPS
+if [[ $? -ne 0 ]]; then
+  echo "Failed to install required applications. Exiting script."
+  exit 1
+fi 
+
+go install github.com/lc/gau/v2/cmd/gau@latest
+if [[ $? -ne 0 ]]; then
+  echo "Failed to install required applications. Exiting script."
+  exit 1
+fi
+cp /root/go/bin/gau /usr/bin/
+
+wget https://github.com/tomnomnom/unfurl/releases/download/v0.0.1/unfurl-linux-amd64-0.0.1.tgz
+if [[ $? -ne 0 ]]; then
+  echo "Failed to download unfurl. Exiting script."
+  exit 1
+fi
+tar xzf unfurl-linux-amd64-0.0.1.tgz
+if [[ $? -ne 0 ]]; then
+  echo "Failed to download unfurl. Exiting script."
+  exit 1
+fi
+sudo mv unfurl /usr/bin
+
+wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
+if [[ $? -ne 0 ]]; then
+  echo "Failed to download unfurl. Exiting script."
+  exit 1
+fi
+unzip aquatone_linux_amd64_1.7.0.zip
+if [[ $? -ne 0 ]]; then
+  echo "Failed to download unfurl. Exiting script."
+  exit 1
+fi
+sudo mv aquatone /usr/bin
+
+git clone https://github.com/Edu4rdSHL/findomain.git
+if [[ $? -ne 0 ]]; then
+  echo "Failed to download unfurl. Exiting script."
+  exit 1
+fi
+cd findomain && cargo build --release
+if [[ $? -ne 0 ]]; then
+  echo "Failed to download unfurl. Exiting script."
+  exit 1
+fi
+sudo cp target/release/findomain /usr/bin/
+
 
 echo "DONE"
 
