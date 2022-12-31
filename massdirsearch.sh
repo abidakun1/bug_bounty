d0bceaba72153cd80b2465ac719825b3d44ecf72
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-12-31 10:13:00 +0100

    Update massdirsearch.sh

--- massdirsearch.sh
+++ massdirsearch.sh
@@ -3,7 +3,7 @@
 #adjust this to fix the path to list of domain or subdomain 
 domain_wordlist="/path/to/sub_wordlist.txt"
 
-cat $wordlist | while read domain; do
+cat $domain_wordlist | while read domain; do
 #adjust gobuster wordlists option to suit your need 
 gobuster dir -u $domain -w /usr/share/dirb/wordlsts/common.txt >> outfile.txt
 
